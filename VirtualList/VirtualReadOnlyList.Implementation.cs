1907eeff86b117108f5cc15eed7d57b46eb36ead
pascal <pascal@craponne.fr>
2023-01-29 18:18:27 +0100

    Add project files.

---
+++ VirtualList/VirtualReadOnlyList.Implementation.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace VirtualList;
+
+partial class VirtualReadOnlyList<TItem> : IReadOnlyList<TItem>
+{
+    private class Enumerator : IEnumerator<TItem>
+    {
+        private readonly VirtualReadOnlyList<TItem> _list;
+        private int _currentIndex;
+
+        public TItem Current => _list[_currentIndex];
+        object? IEnumerator.Current => Current;
+
+        public Enumerator(VirtualReadOnlyList<TItem> list)
+        {
+            _list = list;
+            Reset();
+        }
+
+        public bool MoveNext() => _currentIndex++ < _list.Count;
+        public void Reset() => _currentIndex = -1;
+        public void Dispose() { }
+    }
+
+    public int Count => _size;
+
+    public IEnumerator<TItem> GetEnumerator() => new Enumerator(this);
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public TItem this[int index] => GetAt(index);
+}
---
+++ VirtualList/VirtualReadOnlyList.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VirtualList;
+
+public interface IVirtualListReader<out TItem>
+{
+    public IEnumerable<TItem> Get(int offset, int count);
+}
+
+partial class VirtualReadOnlyList<TItem> 
+{
+    private class Page
+    {
+        public int Index;
+        public TItem[] Data;
+    }
+
+    private readonly IVirtualListReader<TItem> _reader;
+    private readonly int _size;
+
+    private readonly Dictionary<int, Page> _pages = new();
+
+    private int _pageSize;
+
+    public int PageSize
+    {
+        get { return _pageSize; }
+        set
+        {
+            if (_pageSize != value)
+            {
+                _pageSize = value;
+                _pages.Clear();
+            }
+        }
+    }
+
+    public VirtualReadOnlyList(IVirtualListReader<TItem> reader, int size)
+    {
+        _reader = reader;
+        _size = size;
+    }
+
+    private Page GetPage(int index) => TryGetPage(index) ?? LoadPage(index);
+
+    private Page? TryGetPage(int index)
+    {
+        if (_pages.TryGetValue(index, out var page))
+            return page;
+        return null;
+    }
+
+    private Page LoadPage(int index)
+    {
+        var page = new Page { Index = index, Data = GetPage(index * PageSize, PageSize).ToArray() };
+        _pages[index] = page;
+        return page;
+    }
+
+    protected virtual IEnumerable<TItem> GetPage(int offset, int size) => _reader.Get(offset, size);
+
+    private TItem GetAt(int index)
+    {
+        var page = GetPage(index / PageSize);
+        return page.Data[index % PageSize];
+    }
+}
